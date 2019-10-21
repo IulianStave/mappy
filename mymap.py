bfc50f14ef5edd1995b8a79f7e6474e5d16ae7af
iulianstave <iulian.staverescu@gmail.com>
2019-10-21 23:10:46 +0300

    Add another layer - polygon, different color depending on population size

--- mymap.py
+++ mymap.py
@@ -34,15 +34,28 @@ fg = folium.FeatureGroup(name="My map")
 #we use zip function to iterate through two lists
 for lt, ln, el in zip(lat, lon, elev):
       iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-      fg.add_child(folium.Marker(
+      fg.add_child(folium.CircleMarker(#fg.add_child(folium.Marker(
                             location=[lt, ln],
                             #popup=str(el)+" m",
                             popup=folium.Popup(iframe),
-                            icon=folium.Icon(color=color_producer(el))
-                            )
+                            icon=folium.Icon(color=color_producer(el)),
+                            radius=7,
+                            fill_color=color_producer(el),
+                            color='grey',
+                            fill_opacity=0.7)
+                            
+            
             )
+            #fg.add_child(folium.Polygon)
+
+#load GeoJson data to add a polygon layer
+#x represents Features in world.json
+fg.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(), 
+style_function=lambda x: 
+{'fillColor':'green' if x['properties']['POP2005']<10000000 
+else 'orange' if 1000000<= x['properties']['POP2005']<20000000 else 'red'}))
+
+
+
 map.add_child(fg)
-
-
-
 map.save("Map1.html")
