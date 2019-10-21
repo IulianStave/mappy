6f44ac72daf0e982acabf0c8201b1973050c6712
iulianstave <iulian.staverescu@gmail.com>
2019-10-21 23:18:21 +0300

    Separate the volcanoes layer from population layer, ControlLayer

--- mymap.py
+++ mymap.py
@@ -28,13 +28,13 @@ map = folium.Map(location=[38.58,-99.09], zoom_start=5, tiles="Stamen Terrain")
 #add markers
 
 #map.add_child(folium.Marker(location=[38.2,-99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
-fg = folium.FeatureGroup(name="My map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 #for coordinates in [[38.2,-99.1],[37.2,-98.1]]:
 
 #we use zip function to iterate through two lists
 for lt, ln, el in zip(lat, lon, elev):
       iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-      fg.add_child(folium.CircleMarker(#fg.add_child(folium.Marker(
+      fgv.add_child(folium.CircleMarker(#fg.add_child(folium.Marker(
                             location=[lt, ln],
                             #popup=str(el)+" m",
                             popup=folium.Popup(iframe),
@@ -50,12 +50,16 @@ for lt, ln, el in zip(lat, lon, elev):
 
 #load GeoJson data to add a polygon layer
 #x represents Features in world.json
-fg.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(), 
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(), 
 style_function=lambda x: 
 {'fillColor':'green' if x['properties']['POP2005']<10000000 
 else 'orange' if 1000000<= x['properties']['POP2005']<20000000 else 'red'}))
 
 
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
